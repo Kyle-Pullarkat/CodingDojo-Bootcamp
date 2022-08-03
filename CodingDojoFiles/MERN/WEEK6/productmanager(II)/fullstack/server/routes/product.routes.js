437da33a6164c82546ddd0ac20beaf63fd67f116
Kyle-Pullarkat <kpullarkat@yahoo.com>
2022-08-03 15:20:34 -0700

    removing mern for now

--- CodingDojoFiles/MERN/WEEK6/productmanager(II)/fullstack/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const productController = require('../controllers/product.controller');
-
-module.exports = (app) => {
-    app.get('/api/product', productController.getProducts);
-    app.get('/api/product/:id', productController.getProductById);
-    app.post('/api/product', productController.createProduct);
-    app.put('/api/product/:id', productController.updateProduct);
-    app.delete('/api/product/:id', productController.deleteProduct);
-};
